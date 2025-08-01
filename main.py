172c97e9a04f862df85c5b9973604a9a7e16211b
bwhenry99 <bwhenry19@gmail.com>
2025-07-31 21:56:07 -0600

    initial draw of player character

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from constants import *
 import pygame
+from player import *
 
 def main():
     pygame.init()
@@ -11,8 +12,11 @@ def main():
     clock = pygame.time.Clock()
     dt = 0;
 
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, PLAYER_RADIUS)
+
     while True:
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
