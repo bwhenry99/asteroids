3776fa08700d404169d8d14e1aa1e643c0970d46
bwhenry99 <bwhenry19@gmail.com>
2025-08-02 19:56:35 -0600

    added ability for player to shoot

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pygame
 from player import *
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     pygame.init()
@@ -12,21 +13,24 @@ def main():
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0;
+    dt = 0
 
     # Create groups
     updateable_group = pygame.sprite.Group()
     drawable_group = pygame.sprite.Group()
-    asteroid_group =pygame.sprite.Group()
+    asteroid_group = pygame.sprite.Group()
+    shot_group = pygame.sprite.Group()
 
     # create player
     Player.containers = (updateable_group, drawable_group)
+    Shot.containers = (updateable_group, drawable_group, shot_group)
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, PLAYER_RADIUS)
 
     # create asteroids
     Asteroid.containers = (updateable_group, drawable_group, asteroid_group)
     AsteroidField.containers = (updateable_group)
     field = AsteroidField()
+
 
     while True:
         screen.fill("black")
--- player.py
+++ player.py
@@ -1,6 +1,7 @@
 from circleshape import CircleShape
 import pygame
 from constants import *
+from shot import Shot
 
 class Player(CircleShape):
     def __init__(self, x, y, radius):
@@ -18,6 +19,8 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(-dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot()
 
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -36,3 +39,9 @@ class Player(CircleShape):
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
+
+    def shoot(self):
+        new_shot = Shot(self.position[0], self.position[1], SHOT_RADIUS)
+        velocity = pygame.Vector2(0,1)
+        velocity = velocity.rotate(self.rotation)
+        new_shot.velocity = velocity * SHOT_SPEED
